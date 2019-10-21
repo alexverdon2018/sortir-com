a30f26c64acacbb791426348fc108f9c893d0a9a
Alex Verdon <alex.verdon2018@campus-eni.fr>
2019-10-21 14:59:12 +0200

    login v0.2.0

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
@@ -24,8 +26,6 @@ class SecurityController extends AbstractController
     {
 
         return $this->render('security/login.html.twig', array(
-            'last_username' => $lastUsername,
-            'error'         => $error,
         ));
 
     }

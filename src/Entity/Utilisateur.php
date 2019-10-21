53f9ca8de305930470b7b0892739db563524206c
Alex Verdon <alex.verdon2018@campus-eni.fr>
2019-10-21 15:31:08 +0200

    Update Utilisateur.php

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -140,22 +140,7 @@ class Utilisateur implements UserInterface
 
         return $this;
     }
-
-<<<<<<< HEAD
-    /**
-     * Returns the roles granted to the user.
-     *
-     *     public function getRoles()
-     *     {
-     *         return ['ROLE_USER'];
-     *     }
-     *
-     * Alternatively, the roles might be stored on a ``roles`` property,
-     * and populated in any number of different ways when the user object
-     * is created.
-     *
-     * @return (Role|string)[] The user roles
-     */
+    
     public function getRoles()
     {
         // TODO: Implement getRoles() method.
@@ -193,49 +178,5 @@ class Utilisateur implements UserInterface
     {
         // TODO: Implement eraseCredentials() method.
     }
-=======
-    // UserInterface methods
-    public function loadUserByUsername($username)
-    {
-        // Load a User object from your data source or throw UsernameNotFoundException.
-        // The $username argument may not actually be a username:
-        // it is whatever value is being returned by the getUsername()
-        // method in your User class.
-        throw new \Exception('TODO: fill in loadUserByUsername() inside '.__FILE__);
-    }
 
-    /**
-     * Refreshes the user after being reloaded from the session.
-     *
-     * When a user is logged in, at the beginning of each request, the
-     * User object is loaded from the session and then this method is
-     * called. Your job is to make sure the user's data is still fresh by,
-     * for example, re-querying for fresh User data.
-     *
-     * If your firewall is "stateless: true" (for a pure API), this
-     * method is not called.
-     *
-     * @return UserInterface
-     */
-    public function refreshUser(UserInterface $user)
-    {
-        if (!$user instanceof User) {
-            throw new UnsupportedUserException(sprintf('Invalid user class "%s".', get_class($user)));
-        }
-
-        // Return a User object after making sure its data is "fresh".
-        // Or throw a UsernameNotFoundException if the user no longer exists.
-        throw new \Exception('TODO: fill in refreshUser() inside '.__FILE__);
-    }
-
-    /**
-     * Tells Symfony to use this provider for this User class.
-     */
-    public function supportsClass($class)
-    {
-        return User::class === $class;
-    }
-
-
->>>>>>> b3e73c1ceffc06e4089436c78f7b1fa58b7af79d
 }

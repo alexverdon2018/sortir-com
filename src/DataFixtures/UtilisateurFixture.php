11e1dc03102504c5353eacf8279fde4e9319b185
Patrick <patrickxavierrichard@gmail.com>
2019-10-22 10:02:30 +0200

    fixture users
    1 admin 10 users generated

--- src/DataFixtures/UtilisateurFixture.php
+++ src/DataFixtures/UtilisateurFixture.php
@@ -6,10 +6,12 @@ use App\Entity\Utilisateur;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Faker;
 
 class UtilisateurFixture extends Fixture
 {
     private $passwordEncoder;
+
     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
     {
         $this->passwordEncoder = $passwordEncoder;
@@ -22,11 +24,13 @@ class UtilisateurFixture extends Fixture
      */
     public function load(ObjectManager $manager)
     {
-        $manager->persist($this->createUser());
+        $manager->persist($this->createAdmin());
+        $this->createUsers($manager);
         $manager->flush();
     }
 
-    public function createUser() {
+    public function createAdmin()
+    {
         $user = new Utilisateur();
         $user->setMail(sprintf('test@test.com'));
         $user->setNom("test");
@@ -39,4 +43,27 @@ class UtilisateurFixture extends Fixture
         ));
         return $user;
     }
+
+    public function createUsers(ObjectManager $manager) {
+        // On configure dans quelles langues nous voulons nos données
+        $faker = Faker\Factory::create('fr_FR');
+
+        // on créé 10 personnes
+        for ($i = 0; $i < 10; $i++) {
+            $user = new Utilisateur();
+            $user->setNom($faker->name);
+            $user->setPrenom($faker->name);
+            $user->setMail($faker->email);
+            $user->setActif(true);
+            $user->setAdmin(false);
+            $user->setPassword($this->passwordEncoder->encodePassword(
+                $user,
+                'test'
+            ));
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+
 }

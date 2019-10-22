03e6cdb86a4d5bbfcd792065bb7e75eee0cf68c4
Alex Verdon <alex.verdon2018@campus-eni.fr>
2019-10-22 12:06:09 +0200

    Afficher profil 1.0.0
    /profil/{id} permet d'afficher le profil d'un utilisateur
    
    Si c'est le meme id que celui de l'utilisateur connecté, le bouton "Modifier" est visible

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,18 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Utilisateur;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfilController extends AbstractController
 {
     /**
-     * @Route("/profil", name="profil")
+     * @Route("/profil/{id}", name="profil")
      */
-    public function index()
+    public function index($id, EntityManagerInterface $emi)
     {
+        $user = $emi->getRepository(Utilisateur::class)->find($id);
+        if ($user==null) {
+            throw $this->createNotFoundException("User not found");
+        }
         return $this->render('profil/index.html.twig', [
             'controller_name' => 'ProfilController',
+            'user_profil' => $user
         ]);
     }
 }

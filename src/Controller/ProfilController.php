b5cb2300847b80207f7c7b01ed0b0b9249571c33
Alex Verdon <alex.verdon2018@campus-eni.fr>
2019-10-22 15:28:01 +0200

    IHM navbar / create UpdateUtilisateurType / update UtilisateurController

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -3,24 +3,44 @@
 namespace App\Controller;
 
 use App\Entity\Utilisateur;
+use App\Form\UpdateUtilisateurType;
 use Doctrine\ORM\EntityManagerInterface;
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfilController extends AbstractController
 {
     /**
-     * @Route("/profil/{id}", name="profil")
+     * @Route("/profil/{id}", name="profil_detail")
      */
-    public function index($id, EntityManagerInterface $emi)
+    public function detail($id, EntityManagerInterface $emi)
     {
         $user = $emi->getRepository(Utilisateur::class)->find($id);
         if ($user==null) {
-            throw $this->createNotFoundException("User not found");
+            throw $this->createNotFoundException("L'utilisateur est absent dans la base de données. Essayez un autre ID !");
         }
         return $this->render('profil/index.html.twig', [
             'controller_name' => 'ProfilController',
             'user_profil' => $user
         ]);
     }
+
+//    /**
+//     * @Route("/profil/update/{id}", name="profil_update")
+//     */
+//    public function update(Request $request, EntityManagerInterface $em) {
+//        $user = new User();
+//        $formUser = $this->createForm(UpdateUtilisateurType::class, $user);
+//        $formUser->handleRequest($request);
+//        if ($formUser->isSubmitted() && $formUser->isValid()) {
+//            $em->persist($user);
+//            $em->flush();
+//            $this->addFlash('success', "Profil modifié avec succès !");
+//            return $this->redirectToRoute("profil_detail", ["id"=>$user->getId()]);
+//        }
+//        return $this->render("profil/update.html.twig", [
+//            'formUser' => $formUser->createView()
+//        ]);
+//    }
 }

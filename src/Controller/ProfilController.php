a011693c726173d64010a50554bcabf16edeb4ea
Alex Verdon <alex.verdon2018@campus-eni.fr>
2019-10-22 10:54:58 +0200

    IHM Navbar

---
+++ src/Controller/ProfilController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfilController extends AbstractController
+{
+    /**
+     * @Route("/profil", name="profil")
+     */
+    public function index()
+    {
+        return $this->render('profil/index.html.twig', [
+            'controller_name' => 'ProfilController',
+        ]);
+    }
+}

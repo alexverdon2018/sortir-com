5119eae5e7e35114e601c1d780fa8e00c629e30a
Patrick <patrickxavierrichard@gmail.com>
2019-10-23 11:18:39 +0200

    Update filtres.js

--- public/js/filtres.js
+++ public/js/filtres.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
+    const siteInput = document.querySelector('#inputGroup_site');
+    const siteNameInput = document.querySelector('#inputGroup_nom');
+    const sortieDateDebutInput = document.querySelector('#inputGroup_dateDebut');
+    const sortieDateFin = document.querySelector('#inputGroup_dateFin');
+    
+
     const trs = document.querySelector('tbody');
     debugger;
 }, false);

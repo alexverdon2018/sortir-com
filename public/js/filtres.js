e55c8c20c8f0312064f3bd8299ce1959a316857e
Patrick <patrickxavierrichard@gmail.com>
2019-10-23 16:26:38 +0200

    Update filtres.js

--- public/js/filtres.js
+++ public/js/filtres.js
@@ -5,6 +5,7 @@
         const sortieDateDebutInputVal = document.querySelector('#inputGroup_dateDebut').value;
         const sortieDateFinVal = document.querySelector('#inputGroup_dateFin').value;
         const jeSuisOrgaCheckbox = document.querySelector('#checkbox_jeSuisOrga');
+        const jeSuisinscritCheckbox = document.querySelector("#checkbox_jeSuisInsc");
         const trs = [...document.querySelector('tbody').children];
         debugger;
         // Valeur initiale de filteredTrs
@@ -14,7 +15,6 @@
             const siteNameInputVal = evt.currentTarget.value;
             trs.forEach((tr) => {
                 filteredTrs = filteredTrs.map((tr) => {
-                    tr.style.display = 'table-row';
                     if (siteNameInputVal) {
                         tr.children[0].textContent.includes(siteNameInputVal) ?
                             tr.style.display = 'table-row' : tr.style.display = 'none';
@@ -25,8 +25,9 @@
         });
 
       jeSuisOrgaCheckbox.addEventListener('change', evt => {
+          debugger;
           filteredTrs = filteredTrs.map((tr) => {
-              tr.style.display = 'table-row';
+
               const showSelfOrga = evt.currentTarget.checked;
               const userName = document.querySelector("#orga_full_name").textContent;
               const trOrga = tr.children[6].textContent;
@@ -36,8 +37,17 @@
               } else if (showSelfOrga && (userName !== trOrga)) {
                   tr.style.display = 'none';
               };
+              debugger;
               return tr;
           });
       });
-      document.querySelector('tbody').children = filteredTrs;
+
+      jeSuisinscritCheckbox.addEventListener('change', evt => {
+          filteredTrs = filteredTrs.map((tr) => {
+              tr.style.display = 'table-row';
+              const showSelfRegistered = evt.currentTarget.checked;
+              debugger;
+          });
+      });
+      //document.querySelector('tbody').children = filteredTrs;
 }, false);

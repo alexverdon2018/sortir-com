21cd959837faa45a1dcea4a2b1b0b46977421db2
Patrick <patrickxavierrichard@gmail.com>
2019-10-23 09:26:34 +0200

    les sorties
    index

--- src/Controller/ListeSortiesController.php
+++ src/Controller/ListeSortiesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +11,12 @@ class ListeSortiesController extends AbstractController
     /**
      * @Route("/liste/sorties", name="liste_sorties")
      */
-    public function index()
+    public function index(EntityManagerInterface $emi)
     {
+        $sorties = $emi->getRepository(Sortie::class)->findAll();
         return $this->render('liste_sorties/index.html.twig', [
             'controller_name' => 'ListeSortiesController',
+            'sorties' => $sorties
         ]);
     }
 }

000880aef1eb1db89284a61ab54ae62b67e0103a
Patrick <patrickxavierrichard@gmail.com>
2019-10-23 15:19:24 +0200

    sauver le soldat js
    zae

--- public/js/filtres.js
+++ public/js/filtres.js
@@ -1,20 +1,48 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const siteInput = document.querySelector('#inputGroup_site');
-    const siteNameInputVal = document.querySelector('#inputGroup_nom');
-    const sortieDateDebutInputVal = document.querySelector('#inputGroup_dateDebut').value;
-    const sortieDateFinVal = document.querySelector('#inputGroup_dateFin').value;
-    const trs = [...document.querySelector('tbody').children];
-    debugger;
+@@ -1,20 +1,44 @@
+    document.addEventListener('DOMContentLoaded', function(){
+        const siteInput = document.querySelector('#inputGroup_site');
+        const siteNameInputVal = document.querySelector('#inputGroup_nom');
+        const siteNameInput = document.querySelector('#inputGroup_nom');
+        const sortieDateDebutInputVal = document.querySelector('#inputGroup_dateDebut').value;
+        const sortieDateFinVal = document.querySelector('#inputGroup_dateFin').value;
+        const jeSuisOrgaCheckbox = document.querySelector('#checkbox_jeSuisOrga');
+        const trs = [...document.querySelector('tbody').children];
+        debugger;
+        // Valeur initiale de filteredTrs
+        let filteredTrs = [...trs];
 
-    siteNameInputVal.addEventListener('keyup', evt =>{
-        const siteNameInputVal = evt.currentTarget.value;
-        trs.forEach((tr) => {
-            tr.style.display = 'table-row';
-            if (siteNameInputVal) {
-                tr.children[0].textContent.includes(siteNameInputVal) ?
-                    tr.style.display = 'table-row' : tr.style.display = 'none';
-            }
-        });
-    });
-}, false);
+        siteNameInputVal.addEventListener('keyup', evt =>{
 
+            siteNameInput.addEventListener('keyup', evt =>{
+                const siteNameInputVal = evt.currentTarget.value;
+                trs.forEach((tr) => {
+                    filteredTrs = filteredTrs.map((tr) => {
+                        tr.style.display = 'table-row';
+                        if (siteNameInputVal) {
+                            tr.children[0].textContent.includes(siteNameInputVal) ?
+                                tr.style.display = 'table-row' : tr.style.display = 'none';
+                        }
+                        return tr;
+                    });
+                });
+
+                jeSuisOrgaCheckbox.addEventListener('change', evt => {
+                    filteredTrs = filteredTrs.map((tr) => {
+                        tr.style.display = 'table-row';
+                        const showSelfOrga = evt.currentTarget.checked;
+                        const userName = document.querySelector("#orga_full_name").textContent;
+                        const trOrga = tr.children[6].textContent;
+                        if (showSelfOrga && (userName === trOrga)) {
+                            tr.style.display = 'table-row';
+                        } else if (showSelfOrga && (userName !== trOrga)) {
+                            tr.style.display = 'none';
+                        };
+                        return tr;
+                    });
+
+                });
+
+                document.querySelector('tbody').children = filteredTrs;
+
+
+            }, false);

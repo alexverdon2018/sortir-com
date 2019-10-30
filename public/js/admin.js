efb155e0cbcae10fc924d313d3418fb50555c8db
Patrick <patrickxavierrichard@gmail.com>
2019-10-30 15:07:21 +0100

    Update admin.js
    js input import csv

--- public/js/admin.js
+++ public/js/admin.js
@@ -67,5 +67,14 @@ document.addEventListener('DOMContentLoaded', () =>{
         }
     });
 
+    const csvInput = document.querySelector("#import_users_file_csv");
+    const csvLabel = document.querySelector(".custom-file-label");
+    csvInput.addEventListener('change', evt => {
+        const { value } = evt.currentTarget;
+        if (value) {
+            const fileName = value.split("\\").pop() || value.split("/").pop();
+            csvLabel.innerHTML = fileName;
+        }
+    });
 
 }, false);

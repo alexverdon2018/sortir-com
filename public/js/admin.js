d769e57506db907e9c5c85ea35ff63bcb177edf9
Patrick <patrickxavierrichard@gmail.com>
2019-10-30 09:35:17 +0100

    filtres panel admin
    + miku

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () =>{
     const userSearchBar = document.querySelector("#searchUser");
     const villeSearchBar = document.querySelector("#searchVille");
+    const siteSearchBar = document.querySelector("#searchSite");
 
     userSearchBar.addEventListener("keyup", evt => {
         const { children } = document.querySelector("#v-pills-tab");
@@ -9,15 +10,15 @@ document.addEventListener('DOMContentLoaded', () =>{
             return tab.classList.contains("active");
         })[0];
         const { value } = evt.currentTarget;
-        const { children: rowHTMLElements } = document.querySelector("tbody");
+        const { children: rowHTMLElements } = document.querySelector("#v-pills-utilisateurs  tbody");
         const rowArr = [...rowHTMLElements];
         if (activeTab.textContent === "Utilisateurs") {
             rowArr.forEach( row => {
-               row.style.display = 'table-row';
-               const rowUserVal = row.children[0].innerText;
-               rowUserVal.toLowerCase().includes(value.toLowerCase())
-                   ? row.style.display = 'table-row'
-                   : row.style.display = 'none';
+                row.style.display = 'table-row';
+                const rowUserVal = row.children[0].innerText;
+                rowUserVal.toLowerCase().includes(value.toLowerCase())
+                    ? row.style.display = 'table-row'
+                    : row.style.display = 'none';
             });
         }
     });
@@ -29,11 +30,30 @@ document.addEventListener('DOMContentLoaded', () =>{
             return tab.classList.contains("active");
         })[0];
         const { value } = evt.currentTarget;
-        const { children: rowHTMLElements } = document.querySelector("tbody");
+        const { children: rowHTMLElements } = document.querySelector("#v-pills-villes  tbody");
         const rowArr = [...rowHTMLElements];
         if (activeTab.textContent === "Villes") {
             rowArr.forEach( row => {
-                debugger;
+                row.style.display = 'table-row';
+                const rowUserVal = row.children[0].innerText;
+                rowUserVal.toLowerCase().includes(value.toLowerCase())
+                    ? row.style.display = 'table-row'
+                    : row.style.display = 'none';
+            });
+        }
+    });
+
+    siteSearchBar.addEventListener("keyup", evt => {
+        const { children } = document.querySelector("#v-pills-tab");
+        const tabsArr = [...children];
+        const activeTab = tabsArr.filter((tab) => {
+            return tab.classList.contains("active");
+        })[0];
+        const { value } = evt.currentTarget;
+        const { children: rowHTMLElements } = document.querySelector("#v-pills-sites  tbody");
+        const rowArr = [...rowHTMLElements];
+        if (activeTab.textContent === "Sites") {
+            rowArr.forEach( row => {
                 row.style.display = 'table-row';
                 const rowUserVal = row.children[0].innerText;
                 rowUserVal.toLowerCase().includes(value.toLowerCase())
